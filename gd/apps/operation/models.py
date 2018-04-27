4aa61f20b3a3ce418fa1ef4407f48bfdc667ac18
FFFFFhb <fenghaobin@qq.com>
2018-04-27 17:34:06 +0800

    20180427

--- gd/apps/operation/models.py
+++ gd/apps/operation/models.py
@@ -7,7 +7,7 @@ from article.models import Article
 # Create your models here.
 
 class ArticleComments(models.Model):
-    user = models.ForeignKey(UserProfile, verbose_name=u'用户')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name=u'用户')
     article = models.ForeignKey(Article, verbose_name=u'文章')
     comments = models.CharField(max_length=200, verbose_name=u'评论')
     fav_time = models.IntegerField(default=0,verbose_name=u'点赞数量')
@@ -19,7 +19,7 @@ class ArticleComments(models.Model):
 
 
 class UserFavorite(models.Model):
-    user = models.ForeignKey(UserProfile, verbose_name=u'用户')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name=u'用户')
     fav_id = models.IntegerField(default=0, verbose_name=u'数据id')
     fav_type = models.IntegerField(choices=((1,'文章'),(2,'作者')), default=1, verbose_name=u'收藏类型')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'收藏时间')
